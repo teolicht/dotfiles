d5275f4a5d84d4169ed411270c803551c5ccdb85
Teodor de Fine Licht <teodor@musicmedia.dk>
2025-02-28 23:59:23 +0100

    Add tokyonight color palette

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -1,29 +1,38 @@
-#!/bin/sh
+#!/bin/bash
 
 # Black -> white 
-BLACK=0xff000000
-BLACK_95=0xff0d0d0d
-BLACK_90=0xff1a1a1a
-BLACK_85=0xff262626
-BLACK_80=0xff333333
-BLACK_75=0xff404040
-BLACK_70=0xff4d4d4d
-BLACK_65=0xff595959
-BLACK_60=0xff666666
-BLACK_55=0xff737373
-BLACK_50=0xff808080
-BLACK_45=0xff8c8c8c
-BLACK_40=0xff999999
-BLACK_35=0xffa6a6a6
-BLACK_30=0xffb3b3b3
-BLACK_25=0xffbfbfbf
-BLACK_20=0xffcccccc
-BLACK_15=0xffd9d9d9
-BLACK_10=0xffe6e6e6
-BLACK_5=0xfff2f2f2
-WHITE=0xffffffff
-TRANSPARENT=0x00000000
+export BLACK_100=0xff000000
+export BLACK_95=0xff0d0d0d
+export BLACK_90=0xff1a1a1a
+export BLACK_85=0xff262626
+export BLACK_80=0xff333333
+export BLACK_75=0xff404040
+export BLACK_70=0xff4d4d4d
+export BLACK_65=0xff595959
+export BLACK_60=0xff666666
+export BLACK_55=0xff737373
+export BLACK_50=0xff808080
+export BLACK_45=0xff8c8c8c
+export BLACK_40=0xff999999
+export BLACK_35=0xffa6a6a6
+export BLACK_30=0xffb3b3b3
+export BLACK_25=0xffbfbfbf
+export BLACK_20=0xffcccccc
+export BLACK_15=0xffd9d9d9
+export BLACK_10=0xffe6e6e6
+export BLACK_5=0xfff2f2f2
+export WHITE=0xffffffff
+
+export TRANSPARENT=0x00000000
 
 
-TOKYONIGHT_8=0xff414868
-
+# Tokyonight colors
+export RED=0xfff7768e
+export YELLOW=0xffe0af68
+export GREEN=0xff9ece6a
+export BLUE=0xff7aa2f7
+export PURPLE=0xffbb9af7
+export AQUA=0xff7dcfff
+export LIGHT_GRAY=0xffc0caf5
+export DARK_GRAY=0xff414868
+export BLACK=0xff15161e
