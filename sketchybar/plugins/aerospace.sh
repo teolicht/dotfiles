0df4234925a64d3b9990fd52b426a301dd35cdb0
Teodor de Fine Licht <teodor@musicmedia.dk>
2025-03-01 18:53:02 +0100

    Make it so that workspaces without windows are dimmed out

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -2,13 +2,29 @@
 
 source "$CONFIG_DIR/colors.sh"
 
-if [[ "$1" == "$FOCUSED_WORKSPACE" ]]; then
-    sketchybar --set "$NAME" \
-        label.font.style="Black" \
-        background.color="$BLACK_60"
+
+if [[ "$SENDER" == "space_windows_change" ]]; then
+    for sid in $(aerospace list-workspaces --all); do
+        if [[ $(aerospace list-windows --workspace "$sid" | wc -l) -eq 0 ]]; then
+            sketchybar --set space."$sid" \
+              label.color=0x4dffffff # white 30% opacity 
+        else
+            sketchybar --set space."$sid" \
+              label.color="$WHITE"
+        fi
+    done
 else
-    sketchybar --set "$NAME" \
-        label.font.style="Black" \
-        background.color="$BLACK_80"
+  if [[ "$1" == "$FOCUSED_WORKSPACE" ]]; then
+      sketchybar --set "$NAME" \
+          label.font.style="Black" \
+          background.border_color="$PURPLE" \
+          background.height=32 \
+          background.border_width=2 
+  else
+      sketchybar --set "$NAME" \
+          label.font.style="Black" \
+          background.height=34 \
+          background.border_width=0
+  fi
 fi
 
