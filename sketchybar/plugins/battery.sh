2dd7c2f2e3c3a53c010e116771a155cbac7d66a7
Teodor de Fine Licht <teodor@musicmedia.dk>
2025-03-01 18:52:05 +0100

    Add color to battery icon depending on percentage

--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -1,40 +1,66 @@
 #!/bin/bash
 
-source "colors.sh"
+source "$CONFIG_DIR/colors.sh"
 
 PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
 CHARGING="$(pmset -g batt | grep 'AC Power')"
+
+# Overriding so battery icon doesn't have transparency
+RED=0xfff7768e
+YELLOW=0xffe0af68
+GREEN=0xff9ece6a
 
 if [ "$PERCENTAGE" = "" ]; then
   exit 0
 fi
 
-COLOR="$GREEN"
-case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=􀛨   
-  ;;
-  [6-8][0-9]) ICON=􀺸
-  ;;
-  [4-5][0-9]) 
-    ICON=􀺶
-    COLOR="$WHITE"
-  ;;
-  [2-3][0-9])
-    ICON=􀺶
-    COLOR="$YELLOW"
-  ;;
-  [1-2][0-9]) 
-    ICON=􀛩
-    COLOR="$RED"
-  ;;
-  *) 
-    ICON=􀛪
-    COLOR="$RED"
-esac
+# COLOR="$GREEN"
+# case "${PERCENTAGE}" in
+#   9[0-9]|100) ICON=􀛨   
+#   ;;
+#   [6-8][0-9]) ICON=􀺸
+#   ;;
+#   [4-5][0-9]) 
+#     ICON=􀺶
+#     COLOR="$WHITE"
+#   ;;
+#   [2-3][0-9])
+#     ICON=􀺶
+#     COLOR="$YELLOW"
+#   ;;
+#   [1-2][0-9]) 
+#     ICON=􀛩
+#     COLOR="$RED"
+#   ;;
+#   *) 
+#     ICON=􀛪
+#     COLOR="$RED"
+# esac
 
+if [[ "$PERCENTAGE" -ge 90 ]]; then
+  ICON=􀛨
+elif [[ "$PERCENTAGE" -ge 60 ]]; then
+  ICON=􀺸
+elif [[ "$PERCENTAGE" -ge 40 ]]; then
+  ICON=􀺶
+elif [[ "$PERCENTAGE" -ge 10 ]]; then
+  ICON=􀛩
+else
+  ICON=􀛪
+fi
+  
 if [[ "$CHARGING" != "" ]]; then
   ICON=􀢋
+fi
+
+if [[ "$PERCENTAGE" -ge 60 ]]; then
+  COLOR="$GREEN"
+elif [[ "$PERCENTAGE" -ge 40 ]]; then
   COLOR="$WHITE"
+elif [[ "$PERCENTAGE" -ge 20 ]]; then
+  COLOR="$YELLOW"
+else
+  COLOR="$RED"
 fi
 
 # The item invoking this script (name $NAME) will get its icon and label
