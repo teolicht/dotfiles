ac5167f00a5ce6823134dce5239817bb02b7db0b
Teodor de Fine Licht <teodor@musicmedia.dk>
2025-03-02 01:24:45 +0100

    Change external monitor top gap to 42

--- aerospace/top_gap.sh
+++ aerospace/top_gap.sh
@@ -6,11 +6,11 @@ external_monitor_connected=$(displayplacer list | grep -i "27 inch" | wc -l)
 config_file="$HOME/.config/aerospace/aerospace.toml"
 
 if [ "$external_monitor_connected" -gt 0 ]; then
-    # External monitor detected, set the gap to 40
-    sed -i '' 's/outer.top = 10/outer.top = 45/' "$config_file"
+    # External monitor detected, set the gap to 42
+    sed -i '' 's/outer.top = 10/outer.top = 42/' "$config_file"
 else
-    # External monitor not detected, revert to 20
-    sed -i '' 's/outer.top = 45/outer.top = 10/' "$config_file"
+    # External monitor not detected, revert to 10
+    sed -i '' 's/outer.top = 42/outer.top = 10/' "$config_file"
 fi
 
 $(aerospace reload-config)
