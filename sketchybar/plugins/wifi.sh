913f7d9035c3e9bd50d0e581066234e41e93580e
Teodor de Fine Licht <teodor@musicmedia.dk>
2025-03-03 18:45:11 +0100

    Add wifi check with polling rate again

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -3,8 +3,7 @@
 source "$CONFIG_DIR/colors.sh"
 
 WIFI_NAME="$(networksetup -listpreferredwirelessnetworks en0 | sed -n '2 p' | tr -d '\t')"
-host="apple.com"
-ping -c1 "$host" &> /dev/null
+ping -c1 "apple.com" &> /dev/null
 if [ $? -eq 0 ]; then
   ICON=􀙇
   COLOR="$WHITE"
