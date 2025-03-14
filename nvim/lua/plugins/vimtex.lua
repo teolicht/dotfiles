e17e4f876b1639fb96a92068d0b41e3eb7e38167
Teodor de Fine Licht <teodor@musicmedia.dk>
2025-03-14 14:09:57 +0100

    Add instructions for inverse search in Skim

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,9 +1,13 @@
 return {
   'lervag/vimtex',
-  ft = 'tex',
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
-    -- VimTeX configuration
+    -- To make Skim inverse search work with Vimtex:
+    -- 1. Skim -> Settings -> Sync
+    -- 2. Under 'PDF-TeX Sync Support': choose Preset -> Custom
+    -- 3. Command: /opt/homebrew/bin/nvim
+    -- 4. Arguments: --headless -c "VimtexInverseSearch %line '%file'"
+    -- 5. Shift+Command+Click anywhere on PDF file, and cursor on Neovim should jump there
     vim.g.vimtex_view_method = 'skim'
     vim.g.vimtex_compiler_latexmk = {
       options = {
